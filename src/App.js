1ab3f1848e19783807de2923b744d1b94947721a
richard-mcghee <richardjmcghee@yahoo.com>
2020-08-03 18:15:27 -0400

    making progress towards end goal, TodoForm component basically finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import TodoForm from './components/TodoForm'
+import TodoList from './components/TodoList'
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super()
+    this.state = []
+  }
+
+  addTask = newTaskName => {
+    const newTask = {
+      task: newTaskName,
+      id: new Date(),
+      completed: false
+    }
+    this.setState([...this.state, newTask])
+  }
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <TodoForm addTask={this.addTask} />
+        <TodoList />
       </div>
     );
   }
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -3,14 +3,33 @@ import React from 'react';
 class TodoForm extends Component {
     constructor() {
         super();
-        this.state = {  }
+        this.state = {
+            task: ""
+        }
     }
+
+    onInputChange = evt => {
+        this.setState({
+            task: evt.target.value
+        })
+    }
+    onFormSubmit = evt => {
+        evt.preventDefault()
+        this.props.addTask(this.state.task)
+        this.setState({
+            task: ""
+        })
+    }
+
     render() { 
         return ( 
             <form>
                 <input type="text"
-                 name="todoInput"
-                 id={new Date()} />
+                 name="task"
+                 placeholder="Enter New Task"
+                 value={this.state.task}
+                 onChange={this.onInputChange} />
+                <button onSubmit={this.onFormSubmit}>Add To List</button>
             </form>
          );
     }
